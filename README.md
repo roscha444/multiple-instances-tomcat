3de2cbbb937ba3321dfdc21828520d338fdc70ae
Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
2020-02-11 16:34:27 +0100

    Squashed commit of the following:
    commit 864e622745077c76310a94627b69f36007005279
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 16:25:58 2020 +0100
    
        check service is running
    
    commit f467731b97fd05d063b529238ab29a72635840a6
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 16:23:06 2020 +0100
    
        check service is running
    
    commit f6f7a03bf23863874b58d00a35ab6874132e3ea6
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 16:21:27 2020 +0100
    
        check service is running
    
    commit 9ce2d381f024915101105058b538be1edd4f0d87
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 16:12:29 2020 +0100
    
        check service is running
    
    commit b779ad521d874048315bce939f0b84bc2e88d9b9
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 16:03:40 2020 +0100
    
        fix JavaPath
    
    commit 99e33c6d6dc1371132d0fff33e7a87023c63b643
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 15:36:58 2020 +0100
    
        fix service
    
    commit 2a5a0a9815b3f44204989621d413b6d37d7eeffd
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Tue Feb 11 15:34:33 2020 +0100
    
        fix service
    
    commit fb9d0a71b6c5287b32fceea496b5881765b6c242
    Author: Robin Schumacher <info@robin-schumacher.com>
    Date:   Sun Feb 9 20:12:59 2020 +0100
    
        Update README.md
    
    commit 7314358bc647efc63ef5be42b4fec92ae233cd16
    Author: Robin Schumacher <info@robin-schumacher.com>
    Date:   Sun Feb 9 20:12:26 2020 +0100
    
        Update README.md
    
    commit 1f692b0c63056075c8580a20993d8e180bd64415
    Author: Robin Schumacher <info@robin-schumacher.com>
    Date:   Sun Feb 9 20:09:04 2020 +0100
    
        Update README.md
    
    commit 6d33f68f0880750e8736c862ab57e21ac83d11bd
    Author: Robin Schumacher <info@robin-schumacher.com>
    Date:   Sun Feb 9 18:07:10 2020 +0100
    
        Update README.md
    
    commit fad43c248b2d47875a1514867847b12df759355b
    Author: Robin Schumacher <robin.schumacher@stud.tu-darmstadt.de>
    Date:   Sun Feb 9 18:03:40 2020 +0100
    
        small fix and READ.ME

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
-# Run Multiple Instances of Apache Tomcat on Linux 
-A simple bash script to install 1 - 5 apache tomcat instances.
-The script creates a separate folder for each instance and changes the ports in the server.xml.
+# Install Multiple Instances of Apache Tomcat on Linux 
+A simple bash script to install 1 - 4 apache tomcat instances.
+The script creates a separate folder for each instance and changes the ports in the server.xml
 
-# planned:
-During installation, it can be selected for each instance whether it should be configured as a service.
+## Run RunInstallation.sh
+
+1. Go to the folder where you want to install the apache tomcat instances. The script creates subfolders for each instance in this folder.
+2. Download script with
+```
+sudo wget https://raw.githubusercontent.com/roscha444/multiple-instances-tomcat/master/RunInstallation.sh
+```
+2. Make script executable
+```
+sudo chmod +x RunInstallation.sh
+```
+3. Run script with 
+```
+sudo ./RunInstallation.sh
+```
+
+## Ports of apache tomcats instances:
+
+Server 1: HTTP Port...........:8080 <br />
+Server 1: HTTPS Port..........:8443 <br />
+Server 1: Shutdown Port.......:8005 <br />
+Server 1: AJP Connector Port..:8009 <br />
+
+Server 2: Change HTTP Connector Port...:8080 to :8081 <br />
+Server 2: Change HTTPS Connector Port..:8443 to :8444 <br />
+Server 2: Change Shutdown Port.........:8005 to :8006 <br />
+Server 2: Change AJP Connector Port....:8009 to :8010 <br />
+
+Server 3: Change HTTP Connector Port...:8080 to :8082 <br />
+Server 3: Change HTTPS Connector Port..:8443 to :8445 <br />
+Server 3: Change Shutdown Port.........:8005 to :8007 <br />
+Server 3: Change AJP Connector Port....:8009 to :8011 <br />
+
+Server 4: Change HTTP Connector Port...:8080 to :8083 <br />
+Server 4: Change HTTPS Connector Port..:8443 to :8446 <br />
+Server 4: Change Shutdown Port.........:8005 to :8008 <br />
+Server 4: Change AJP Connector Port....:8009 to :8012 <br />
+
+## In this build:
+* The script creates a separate folder for each apache tomcat instance and changes the ports in the server.xml. After the installation all instances are started.
+
+## Planned in next build:
+* Select apache tomcat version to install
+* The possibility to choose whether the apache tomcat instances should be automatically configured as a service.
+* Start and Stop all instances with one command
--- RunInstallation.sh
+++ RunInstallation.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-##### Constants
 
+#Here you can set your java path!!!
+
+JavaPath=$JAVA_HOME
+#EXAMPLE: JavaPath=/usr/lib64/jvm/jre-1.8.0-openjdk
+
+#Here you can set the name of the tmp directory
+tmp=tmpTomcatApache
+
+#Here you can set the name of the servers directorys
 ServerDirectory1=apache-tomcat-1
 ServerDirectory2=apache-tomcat-2
 ServerDirectory3=apache-tomcat-3
 ServerDirectory4=apache-tomcat-4
 
+
+################################
 AUTHOR="Robin Schumacher"
-VERSION="0.1"
-AMOUNT=0
+VERSION="0.2"
+###############################
 
 clear
 echo "##########################################################"
 echo ""
-echo "Run Multiple Instances of Apache Tomcat on Linux"
+echo "Install Multiple Instances of Apache Tomcat on Linux"
 echo "VERSION" $VERSION
 echo "Script by" $AUTHOR
 echo ""
@@ -40,23 +50,33 @@ if !(($AMOUNT > 0 && $AMOUNT < 5)); then
 	echo "##########################################################"
 	exit 1
 fi
+
+echo -n "Configure Apache Tomcat Servers as Service? [y] = yes / [n] = no ? "
+read Service
+
+if ($Service = "y" || $Service = "Y"); then
+	Service=true
+	echo "Configure Tomcat Servers as Service!"
+else
+	Service=false
+fi
 echo ""
 
 echo "##########################################################"
 echo ""
-echo "2. Downloading Apache tomcat and extract to /tmp"
+echo "2. Downloading Apache tomcat and extract to temp"
 echo ""
 echo "##########################################################"
 
 echo ""
-mkdir tmp
-cd tmp
+mkdir $tmp
+cd $tmp
 wget https://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz
 
 {
   tar -zxvf apache-tomcat-9.0.30.tar.gz -C ./
 } &> /dev/null
-echo "extract apache-tomcat-9.0.30.tar.gz to tmp!"
+echo "extract apache-tomcat-9.0.30.tar.gz"
 cd ..
 echo ""
 
@@ -69,26 +89,26 @@ echo "##########################################################"
 echo""
 echo "Copy apache tomcat to Server 1!"
 mkdir $ServerDirectory1
-cp -ar tmp/apache-tomcat-9.0.30/* $ServerDirectory1
+cp -ar $tmp/apache-tomcat-9.0.30/* $ServerDirectory1
 
 if (($AMOUNT > 1)); then
 echo "Copy apache tomcat to Server 2!"
 mkdir $ServerDirectory2
-cp -ar tmp/apache-tomcat-9.0.30/* $ServerDirectory2
+cp -ar $tmp/apache-tomcat-9.0.30/* $ServerDirectory2
 fi
 
 if (($AMOUNT > 2)); then
 echo "Copy apache tomcat to Server 3!"
 mkdir $ServerDirectory3
-cp -ar tmp/apache-tomcat-9.0.30/* $ServerDirectory3
+cp -ar $tmp/apache-tomcat-9.0.30/* $ServerDirectory3
 fi
 
 if (($AMOUNT > 3)); then
 echo "Copy apache tomcat to Server 4!"
 mkdir $ServerDirectory4
-cp -ar tmp/apache-tomcat-9.0.30/* $ServerDirectory4
+cp -ar $tmp/apache-tomcat-9.0.30/* $ServerDirectory4
 fi
-rm -r tmp
+rm -r $tmp
 echo ""
 
 
@@ -113,10 +133,10 @@ sed -i s/8080/8081/ server.xml
 sed -i s/8443/8444/ server.xml 
 sed -i s/8005/8006/ server.xml
 sed -i s/8009/8010/ server.xml
-echo "Server 3: Change HTTP Connector Port...*:8080 to *:8081"
-echo "Server 3: Change HTTPS Connector Port..*:8443 to *:8444"
-echo "Server 3: Change Shutdown Port.........*:8005 to *:8006"
-echo "Server 3: Change AJP Connector Port....*:8009 to *:8010"
+echo "Server 2: Change HTTP Connector Port...*:8080 to *:8081"
+echo "Server 2: Change HTTPS Connector Port..*:8443 to *:8444"
+echo "Server 2: Change Shutdown Port.........*:8005 to *:8006"
+echo "Server 2: Change AJP Connector Port....*:8009 to *:8010"
 echo ""
 cd ..
 cd ..
@@ -145,10 +165,10 @@ sed -i s/8080/8083/ server.xml
 sed -i s/8443/8446/ server.xml
 sed -i s/8005/8008/ server.xml
 sed -i s/8009/8012/ server.xml
-echo "Server 3: Change HTTP Connector Port...*:8080 to *:8083"
-echo "Server 3: Change HTTPS Connector Port..*:8443 to *:8446"
-echo "Server 3: Change Shutdown Port.........*:8005 to *:8008"
-echo "Server 3: Change AJP Connector Port....*:8009 to *:8012"
+echo "Server 4: Change HTTP Connector Port...*:8080 to *:8083"
+echo "Server 4: Change HTTPS Connector Port..*:8443 to *:8446"
+echo "Server 4: Change Shutdown Port.........*:8005 to *:8008"
+echo "Server 4: Change AJP Connector Port....*:8009 to *:8012"
 cd ..
 cd ..
 fi
@@ -161,6 +181,7 @@ echo "6. Start apache tomcat servers"
 echo ""
 echo "##########################################################"
 
+if [ $Service = false ]; then
 echo ""
 $ServerDirectory1/bin/startup.sh
 
@@ -179,3 +200,172 @@ if (($AMOUNT > 3)); then
 $ServerDirectory4/bin/startup.sh
 fi
 echo ""
+
+else
+
+d=`pwd`
+
+cd /etc/systemd/system/
+
+cat <<END >tomcat-1.service
+[Unit]
+Description=Tomcat - instance %i
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+User=root
+Group=root
+
+WorkingDirectory=$d/$ServerDirectory1
+
+Environment="JAVA_HOME=$JavaPath"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+
+Environment="CATALINA_PID=$d/$ServerDirectory1/run/tomcat.pid"
+Environment="CATALINA_BASE=$d/$ServerDirectory1"
+Environment="CATALINA_HOME=$d/$ServerDirectory1"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=$d/$ServerDirectory1/bin/startup.sh
+ExecStop=$d/$ServerDirectory1/bin/shutdown.sh
+
+#RestartSec=10
+#Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
+
+if (($AMOUNT > 1)); then
+cat <<END >tomcat-2.service
+[Unit]
+Description=Tomcat - instance %i
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+User=root
+Group=root
+
+WorkingDirectory=$d/$ServerDirectory2/
+
+Environment="JAVA_HOME=$JavaPath"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+
+Environment="CATALINA_PID=$d/$ServerDirectory2/run/tomcat.pid"
+Environment="CATALINA_BASE=$d/$ServerDirectory2"
+Environment="CATALINA_HOME=$d/$ServerDirectory2"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=$d/$ServerDirectory2/bin/startup.sh
+ExecStop=$d/$ServerDirectory2/bin/shutdown.sh
+
+#RestartSec=10
+#Restart=always
+
+[Install]
+WantedBy=multi-user.target
+
+END
+systemctl daemon-reload
+systemctl enable tomcat-2
+sudo service tomcat-2 start
+
+fi
+
+if (($AMOUNT > 2)); then
+cat <<END >tomcat-3.service
+[Unit]
+Description=Tomcat - instance %i
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+User=root
+Group=root
+
+WorkingDirectory=$d/$ServerDirectory3
+
+Environment="JAVA_HOME=$JavaPath"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+
+Environment="CATALINA_PID=$d/$ServerDirectory3/run/tomcat.pid"
+Environment="CATALINA_BASE=$d/$ServerDirectory3"
+Environment="CATALINA_HOME=$d/$ServerDirectory3"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=$d/$ServerDirectory3/bin/startup.sh
+ExecStop=$d/$ServerDirectory3/bin/shutdown.sh
+
+#RestartSec=10
+#Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable tomcat-3
+sudo service tomcat-3 start
+fi
+
+if (($AMOUNT > 3)); then
+cat <<END >tomcat-4.service
+[Unit]
+Description=Tomcat - instance %i
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+User=root
+Group=root
+
+WorkingDirectory=$d/$ServerDirectory4
+
+Environment="JAVA_HOME=$JavaPath"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+
+Environment="CATALINA_PID=$d/$ServerDirectory4/run/tomcat.pid"
+Environment="CATALINA_BASE=$d/$ServerDirectory4"
+Environment="CATALINA_HOME=$d/$ServerDirectory4"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=$d/$ServerDirectory4/bin/startup.sh
+ExecStop=$d/$ServerDirectory4/bin/shutdown.sh
+
+#RestartSec=10
+#Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+systemctl daemon-reload
+systemctl enable tomcat-4.service
+sudo service tomcat-4 start
+fi
+
+systemctl daemon-reload
+systemctl enable tomcat-1
+sudo service tomcat-1 start
+
+echo "Server 1: "; systemctl show -p SubState --value tomcat-1
+
+if (($AMOUNT > 1)); then 
+echo "Server 2: "; systemctl show -p SubState --value tomcat-2
+fi
+
+if (($AMOUNT > 2)); then 
+echo "Server 3: "; systemctl show -p SubState --value tomcat-3
+fi
+
+if (($AMOUNT > 3)); then 
+echo "Server 4: "; systemctl show -p SubState --value tomcat-4
+fi
+
+fi
